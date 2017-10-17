eb33adc91b4ab0ffe13983b2f46bf28d886329e7
Naito <hateshi.naito@gmail.com>
2017-10-17 22:21:11 +0300

    Add duplicate removal.

--- search orar ubb.js
+++ search orar ubb.js
@@ -1,14 +1,26 @@
 // ==UserScript==
 // @name         Seach orar
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  UBB didn't leave 90s, making their sites without a goddamn search bar
 // @author       Paul Forgaci
-// @match        http://www.cs.ubbcluj.ro/files/orar/2016-2/tabelar/I1.html
+// @match        http://www.cs.ubbcluj.ro/files/orar/*/tabelar/*.html
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
 
+function is_dup(item1, item2) {
+    return item1[0].innerText == item2[0].innerText;
+}
+
+function remove_dup(inputs) {
+    var i;
+    for (i = 0; i < inputs.length - 1; i++)
+        if (is_dup(inputs[i], inputs[i + 1])) {
+            inputs.splice(i + 1, 1);
+            --i;
+        }
+}
 
 function search_input(input, inputs) {
     $('tr').each(function(index) {
@@ -28,8 +40,8 @@ function sort_inputs(inputs) {
     while (!ok) {
         ok = true;
         for (var i = 0; i < inputs.length - 1; ++i) {
-            var a = window.good_types.indexOf(inputs[i][0].children[0].innerText);
-            var b = window.good_types.indexOf(inputs[i + 1][0].children[0].innerText);
+            var a = window.days.indexOf(inputs[i][0].children[0].innerText);
+            var b = window.days.indexOf(inputs[i + 1][0].children[0].innerText);
             if (a > b) {
                 var o = inputs[i];
                 inputs[i] = inputs[i + 1];
@@ -57,21 +69,23 @@ function display_inputs(inputs) {
 (function() {
     'use strict';
 
-    window.good_types = ["Luni", "Marti", "Miercuri", "Joi", "Vineri"];
+    window.days = ["Luni", "Marti", "Miercuri", "Joi", "Vineri"];
     window.allofit = $('center')[0].innerHTML;
     var input;
     var inputs;
     $('body').append('<input id="searchbar" style="position: fixed; top: 0; right: 0px;">');
     $('#searchbar').keyup(function (e) {
-        if (e.which === 13) {
+        if (e.which === 13) { // on enter key press
             input = $('#searchbar').val();
             inputs = [];
             $('#searchbar').val("");
         	search_input(input, inputs);
             sort_inputs(inputs);
+            //console.log(inputs[0][0].textContent);
+            console.log(remove_dup(inputs));
             display_inputs(inputs);
         }
-        if (e.which === 27) {
+        if (e.which === 27) { // on esc key press
             $('center')[0].innerHTML = window.allofit;
         }
     });
